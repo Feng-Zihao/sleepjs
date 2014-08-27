b21d9d3a852e5466e83f637dd945b4c0504e44a1
Feng-Zihao <feng.zh.zq@gmail.com>
2014-08-27 11:41:26 +0800

    Encapsulate wrapper and improve README.

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-sleepjs
+SleepJS
 =======
-
 A utility class for sleep for seconds. Zzzzzzzzz.
+(Especially useful in testcase of JavaScript)
+
+Purpose & Usage
+=======
+In most language, it's not hard to do something like
+```
+sleep(1000);    // in milliseconds
+process_1
+sleep(2000);
+process_2
+sleep(3000);
+process_3
+```
+
+But in JavaScript, the only way is using setTimeout.
+One will looks like
+```
+setTimeout(function() {
+    process_1
+    setTimeout(function() {
+        process_2
+        setTimeout(function() {
+            process_3
+        }, 3000);
+    }, 2000);
+}, 1000);
+```
+which is hard to understand the logic, and break your mind.
+
+
+The other will looks like
+```
+setTimeout(function() {
+    process_1
+}, 1000);
+setTimeout(function() {
+    process_2
+}, 3000);
+setTimeout(function() {
+    process_3
+}, 6000);
+
+```
+which is hard to change time duration. Once you chage the time interval,
+all delay need to be recalculated.
+
+
+If you use SleepJS, you'll write
+```
+sleep(1000).then(function() {
+    process_1
+}).sleep(2000).then(function() {
+    process_2
+}).sleep(3000).then(function() {
+    process_3
+}).run();
+```
+which is natural to sequential logic and make your mind clear.
--- sleep.js
+++ sleep.js
@@ -1,34 +1,39 @@
 
 
 sleep = function(milliseconds) {
-    var delay_list = [];
-    var callback_list = [];
-    var _run = function() {
-        if (callback_list.length === delay_list.length - 1) {
-            callback_list.push(function() {});
-        }
-        for (var i = 1; i < delay_list.length; i++) {
-            delay_list[i] = delay_list[i - 1] + delay_list[i];
-        }
-        for (var i = 0; i < delay_list.length; i++) {
-            setTimeout(callback_list[i], delay_list[i]);
+    var wrapper = function() {
+        var delay_list = [];
+        var callback_list = [];
+        var _run = function() {
+            if (callback_list.length === delay_list.length - 1) {
+                callback_list.push(function() {});
+            }
+            for (var i = 1; i < delay_list.length; i++) {
+                delay_list[i] = delay_list[i - 1] + delay_list[i];
+            }
+            for (var i = 0; i < delay_list.length; i++) {
+                setTimeout(callback_list[i], delay_list[i]);
+            };
         };
-    };
-    var _sleep = function(milliseconds) {
-        delay_list.push(milliseconds);
+        var _sleep = function(milliseconds) {
+            delay_list.push(milliseconds);
+            return {
+                then: _then,
+                run   : _run
+            };
+        };
+        var _then = function(callback) {
+            callback_list.push(callback);
+            return {
+                sleep: _sleep,
+                run  : _run
+            };
+        };
         return {
-            action: _action,
-            run   : _run
-        };
-    };
-    var _action = function(callback) {
-        callback_list.push(callback);
-        return {
-            sleep: _sleep,
-            run  : _run
-        };
-    };
-    return _sleep(milliseconds);
+            sleep: _sleep
+        }
+    }
+    return new wrapper().sleep(milliseconds);
 };
 
 
